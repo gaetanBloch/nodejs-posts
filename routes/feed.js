5cd240f4e2f9a355c8624110f5aa79d9c28192aa
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-10 10:26:05 +0200

    Handle feed routes in app.js server and added feed controller to handle the requests

--- routes/feed.js
+++ routes/feed.js
@@ -4,6 +4,7 @@ const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
+// GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
 module.exports = router;
