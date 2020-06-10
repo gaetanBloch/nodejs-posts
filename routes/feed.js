7a9da0c2911a8946b5d917d9b78213fba75d6807
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-10 19:17:23 +0200

    Refactored duplicated code

--- routes/feed.js
+++ routes/feed.js
@@ -5,30 +5,23 @@ const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
+const validatePost = () => {
+  return [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 })
+  ];
+};
+
 // GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
 // POST /feed/posts
-router.post(
-  '/posts',
-  [
-    body('title').trim().isLength({ min: 5 }),
-    body('content').trim().isLength({ min: 5 })
-  ],
-  feedController.createPost
-);
+router.post('/posts', validatePost(), feedController.createPost);
 
 // GET /feed/posts/id
 router.get('/posts/:postId', feedController.getPost);
 
 // PUT /feed/posts/id
-router.put(
-  '/posts/:postId',
-  [
-    body('title').trim().isLength({ min: 5 }),
-    body('content').trim().isLength({ min: 5 })
-  ],
-  feedController.updatePost
-);
+router.put('/posts/:postId', validatePost(), feedController.updatePost);
 
 module.exports = router;
