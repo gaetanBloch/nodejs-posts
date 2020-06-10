55cea9a5b2c5d57a16085db1b8f835a053a0a627
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-10 23:53:48 +0200

    Encrypt the password and create the User on signup request

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 const { forwardError, throwError, validate } = require('./utils');
 
@@ -8,4 +9,20 @@ exports.signup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const name = req.body.name;
+  bcrypt.hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email,
+        password: hashedPassword,
+        name
+      })
+      return user.save();
+    })
+    .then(result => {
+      res.status(201).json({
+        message: 'User created successfully.',
+        userId: result._id
+      })
+    })
+    .catch(err => forwardError(err));
 };
