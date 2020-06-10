818968b234fc9785168294d3c7e48e5df17fd409
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-10 13:44:43 +0200

    Handle Validation error on the Feed Controller and Send Back and Error Response if validation failed

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -16,6 +18,14 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'Validation failed: Entered data is incorrect.',
+      errors: errors.array()
+    });
+  }
+
   const title = req.body.title;
   const content = req.body.content;
 
