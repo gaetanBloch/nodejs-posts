5516259d14c740c2f73aa0b8cc407161eaf0c347
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-10 14:37:27 +0200

    Use Middleware to Handle Errors

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    'Access-Control-Allow-Methods', 
+    'Access-Control-Allow-Methods',
     'GET, POST, PUT, PATCH, DELETE'
   );
   res.setHeader(
@@ -28,9 +28,18 @@ app.use((req, res, next) => {
     'Content-Type, Authorization'
   );
   next();
-})
+});
 
+// Set up Routes
 app.use('/feed', feedRoutes);
+
+// Middleware to handling Errors
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message });
+});
 
 // Connect to MongoDB
 mongoose
