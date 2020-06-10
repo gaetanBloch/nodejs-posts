0b8f4611fc6a80c539cfb1a779899d69bd4d5bf5
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-10 14:25:07 +0200

    Throwing Errors from the Feed Controller

--- controllers/feed.js
+++ controllers/feed.js
@@ -22,10 +22,9 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: 'Validation failed: Entered data is incorrect.',
-      errors: errors.array()
-    });
+    const error = new Error('Validation failed: Entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
   }
 
   const title = req.body.title;
@@ -45,5 +44,10 @@ exports.createPost = (req, res, next) => {
         message: 'Post created successfully',
         post: result
       });
-    }).catch(err => console.log(err));
+    }).catch(err => {
+    if (err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  });
 };
