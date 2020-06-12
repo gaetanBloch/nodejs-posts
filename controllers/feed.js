bdd2a75452735cc31461822592e5573966e1e940
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-12 10:34:49 +0200

    Use Async / Await Syntax for the getPosts method

--- controllers/feed.js
+++ controllers/feed.js
@@ -22,28 +22,24 @@ const checkAuthorization = (post, req) => {
   }
 };
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
   const currentPage = req.query.page || 1;
   const perPage = 2;
 
-  // Count the number of posts
-  let totalItems;
-  Post.find()
-    .countDocuments()
-    .then(count => {
-      totalItems = count;
-      return Post.find()
-        .skip((currentPage - 1) * perPage)
-        .limit(perPage);
-    })
-    .then(posts => {
-      res.status(200).json({
-        message: 'Posts fetched successfully',
-        posts,
-        totalItems
-      });
-    })
-    .catch(err => forwardError(err, next));
+  try {
+    // Count the number of posts
+    const totalItems = await Post.find().countDocuments()
+    const posts = await Post.find()
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);
+    res.status(200).json({
+      message: 'Posts fetched successfully',
+      posts,
+      totalItems
+    });
+  } catch (err) {
+    forwardError(err, next);
+  }
 };
 
 exports.createPost = (req, res, next) => {
