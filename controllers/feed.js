904e7762c2b486f1f298001db23b7651cb090aef
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-12 11:03:08 +0200

    Added creator information when returning the list of posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -30,6 +30,7 @@ exports.getPosts = async (req, res, next) => {
     // Count the number of posts
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
+      .populate('creator')
       .skip((currentPage - 1) * perPage)
       .limit(perPage);
     res.status(200).json({
