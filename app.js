094823961b37d4b3256b9397275f653aa92dc127
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-13 18:38:21 +0200

    Added CORS authorized headers

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Content-Type, Authorization'
+    'Origin, X-requested-With, Content-Type, Authorization'
   );
   next();
 });
